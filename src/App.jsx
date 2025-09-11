9b2f1e6d0a150bf9f16f29e81c5a60582a925dcd
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2025-09-11 18:45:22 -0300

    arreglo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from ".src/components/Login";
+import Home from ".src/components/Home";
 
 function App() {
   return (
