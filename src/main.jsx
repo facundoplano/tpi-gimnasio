bf6f5866f27f470ce6c6bf3a233b06dad03c1870
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2025-10-05 01:02:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx'
+import Login from "./components/Login.jsx";
+import Home from "./components/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
